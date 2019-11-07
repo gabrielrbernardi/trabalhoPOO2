92d930b8a4e814222a1d65b21e47cd100f96b590
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-11-06 21:34:17 -0300

    Delete Quadrilatero.java

--- Quadrilatero.java
+++
@@ -1,9 +0,0 @@
-package trabalho2POO;
-public class Quadrilatero extends QuadrilateroAbstract{
-	public Quadrilatero(int lados[]){
-		for(int i = 0; i < 4; i++) {
-			this.ladosQuadrilatero[i] = lados[i];
-		}
-	}
-	//Se for um quadrilatero comum, os metodos serao os padroes adotados pela classe abstrata
-}
